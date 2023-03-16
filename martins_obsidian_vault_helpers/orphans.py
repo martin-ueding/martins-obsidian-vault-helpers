6702a9736c6ae5519601e880d01080940fdb9c49
Martin Ueding <mu@martin-ueding.de>
2023-03-16 13:57:58 +0100

    Automatically move orphans into the inbox

--- martins_obsidian_vault_helpers/orphans.py
+++ martins_obsidian_vault_helpers/orphans.py
@@ -1,4 +1,5 @@
 import pathlib
+import shutil
 
 
 def report_orphan_attachments(vault: pathlib.Path, link_graph: dict[pathlib.Path, list[pathlib.Path]]) -> None:
@@ -6,11 +7,13 @@ def report_orphan_attachments(vault: pathlib.Path, link_graph: dict[pathlib.Path
     targets = {target for targets in link_graph.values() for target in targets}
     for attachment in attachments:
         if attachment not in targets:
-            print(attachment)
+            shutil.move(attachment, vault / 'Inbox')
 
 
 def report_notes_not_in_structure(vault: pathlib.Path, link_graph: dict[pathlib.Path, list[pathlib.Path]]) -> None:
     targets_of_structure_notes = {target for source, targets in link_graph.items() if (vault / "Structure Notes") in source.parents for target in targets}
     for source in link_graph.keys():
+        if source.parent not in [vault / "Pages", vault / "Structure Notes"]:
+            continue
         if source not in targets_of_structure_notes:
-            print(source)
+            shutil.move(source, vault / 'Inbox')
